dd796bedf33dd734d9d81575d3f8037791f41fb0
Clarhence Oliveros <134087492+Ashrennn@users.noreply.github.com>
2025-05-14 20:15:44 +0800

    LC removal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 import { CaseStudiesComponent } from './case-studies/case-studies.component';
-import { LegalComplianceComponent } from './legal-compliance/legal-compliance.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 
 export const routes: Routes = [
@@ -11,7 +10,6 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent, data: { breadcrumb: 'About' } },
   { path: 'services', component: ServicesComponent, data: { breadcrumb: 'Services' } },
   { path: 'case-studies', component: CaseStudiesComponent, data: { breadcrumb: 'Case Studies' } },
-  { path: 'legal-compliance', component: LegalComplianceComponent, data: { breadcrumb: 'Legal & Compliance' } },
   { path: 'contact-us', component: ContactUsComponent, data: { breadcrumb: 'Contact Us' } },
   { path: '**', redirectTo: '' }
 ]; 
